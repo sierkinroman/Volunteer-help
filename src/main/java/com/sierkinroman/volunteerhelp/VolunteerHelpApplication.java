cf7a8edb6efb536b9ef2501ccdd74c8b617d13c2
Roman Sierkin <serkin99@ukr.net>
2022-11-01 09:07:02 +0200

    initial commit

---
+++ src/main/java/com/sierkinroman/volunteerhelp/VolunteerHelpApplication.java
@@ -0,0 +1,13 @@
+package com.sierkinroman.volunteerhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerHelpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sierkinroman/volunteerhelp/VolunteerHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sierkinroman.volunteerhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerHelpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
